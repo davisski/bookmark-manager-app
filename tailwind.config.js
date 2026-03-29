0062afc6cdb465f1231cb660ec4b05598a25badb
Davis Skipsna <skipsna.davis@gmail.com>
2026-03-29 16:21:06 +0300

    Starter code

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// return {
+//     darkMode: 'class', // svarīgi
+//     theme: {
+//         extend: {
+//             colors: {
+//                 neutral: {
+//                     900: 'var(--color-neutral-900)',
+//                     800: 'var(--color-neutral-800)',
+//                     500: 'var(--color-neutral-500)',
+//                     400: 'var(--color-neutral-400)',
+//                     300: 'var(--color-neutral-300)',
+//                     100: 'var(--color-neutral-100)',
+//                 }
+//             }
+//         }
+//     }
+// }
