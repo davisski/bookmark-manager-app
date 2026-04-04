8cb6b56fef8b59a1eab6ab22d772e05d3f352fbe
Davis Skipsna <skipsna.davis@gmail.com>
2026-04-04 15:44:08 +0300

    #26 - CardDotComponent as well visit, copy and pin functionality

---
+++ app/components/CardDotComponent.tsx
@@ -0,0 +1,101 @@
+
+import { useState } from 'react';
+import darkDots from '../assets/images/icon-dots-dark.svg';
+import darkVisit from '../assets/images/dark-visit.svg';
+import darkCopy from '../assets/images/dark-copy.svg';
+import iconPinDark from '../assets/images/icon-pin-dark.svg';
+import darkEdit from '../assets/images/dark-edit.svg';
+import iconArchiveDark from '../assets/images/icon-archive-dark.svg';
+import darkUnpin from '../assets/images/dark-unpin.svg';
+
+import { useBookmarks } from '~/context/BookmarkContext';
+export const CardDotComponent = ({ bookmark }: { bookmark: any }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const { pinBookmark, unpinBookmark } = useBookmarks();
+    let options = [
+        { label: "Visit", icon: darkVisit, method: "visit" },
+        { label: "Copy URL", icon: darkCopy, method: "copy" },
+        { label: "Pin", icon: iconPinDark, method: "pin" },
+        { label: "Edit", icon: darkEdit, method: "edit" },
+        { label: "Archive", icon: iconArchiveDark, method: "archive" },
+    ];
+
+    if (bookmark.pinned) {
+        options[2] = { label: "Unpin", icon: darkUnpin, method: "pin" };
+    }
+    
+
+    const visit = () => {
+        window.open(bookmark.url, '_blank');
+        setIsOpen(false);
+    }
+
+    const copy = () => {
+        navigator.clipboard.writeText(bookmark.url).then(() => {
+            alert('URL copied to clipboard!');
+            setIsOpen(false);
+        }).catch((err) => {
+            console.error('Could not copy text: ', err);
+        });
+    }
+
+    const pin = () => {
+        console.log("Pin/Unpin method called");
+        if (bookmark.pinned) {
+            unpinBookmark(bookmark.id);
+        } else {
+            pinBookmark(bookmark.id);
+        }
+        setIsOpen(false);
+    }
+
+    const edit = () => {
+        console.log("Edit method called");
+    }
+
+    const archive = () => {
+        console.log("Archive method called");
+    }
+
+    const handleOptionSelect = (method: string) => {
+        switch (method) {
+            case "visit":
+                visit();
+                break;
+            case "copy":
+                copy();
+                break;
+            case "pin":
+            case "unpin":
+                pin();
+                break;
+            case "edit":
+                edit();
+                break;
+            case "archive":
+                archive();
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <>
+            <button onClick={() => setIsOpen(!isOpen)} className='cursor-pointer dark:focus:outline-neutral-100 focus:outline-2 focus:outline-offset-3 dark:bg-neutral-800 border flex items-center justify-center border-neutral-500 rounded-lg w-8 h-8'>
+                <img className='w-5 h-5' src={darkDots} alt="Options" />
+            </button>
+            <div className={`absolute top-13 right-4 overflow-hidden bg-white dark:bg-neutral-600 border border-gray-300 dark:border-neutral-500 rounded-lg shadow-lg min-w-50 min-h-33 p-2 z-10 ${isOpen ? "block" : "hidden"}`}>
+                <ul className="flex flex-col justify-between min-h-33">
+                    {options.map((option) => (
+                        <li onClick={() => handleOptionSelect(option.method)} key={option.method} className="px-4 py-2 hover:bg-gray-100 dark:text-neutral-100 cursor-pointer dark:hover:bg-neutral-800
+                            flex items-center gap-2.5">
+                            <img className='w-4 h-4' src={option.icon} alt={option.label} />
+                            <span>{option.label}</span>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </>
+    )
+}
--- app/context/BookmarkContext.tsx
+++ app/context/BookmarkContext.tsx
@@ -7,41 +7,93 @@ interface BookmarkContextType {
   bookmarks: Bookmark[];
   setBookmarks: React.Dispatch<React.SetStateAction<Bookmark[]>>;
   sortBookmarks: (sortBy: 'createdAt' | 'lastVisited' | 'visitCount', direction?: 'asc' | 'desc') => void;
+  pinBookmark: (id: string) => void;
+  unpinBookmark: (id: string) => void;
+  defaultSort: (bookmarks: Bookmark[]) => void;
 }
 
 const BookmarkContext = createContext<BookmarkContextType | undefined>(undefined);
 
 export const BookmarkProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [bookmarks, setBookmarks] = useState<Bookmark[]>(bookmarksData.bookmarks);
+    
+    const [bookmarks, setBookmarks] = useState<Bookmark[]>(bookmarksData.bookmarks);
 
-  const sortBookmarks = (
-    sortBy: 'createdAt' | 'lastVisited' | 'visitCount',
-    direction: 'asc' | 'desc' = 'asc'
-  ) => {
-    setBookmarks(prev =>
-      [...prev].sort((a, b) => {
-        let aValue = a[sortBy];
-        let bValue = b[sortBy];
-        if (sortBy === 'createdAt' || sortBy === 'lastVisited') {
-          aValue = aValue ? new Date(aValue as string).getTime() : 0;
-          bValue = bValue ? new Date(bValue as string).getTime() : 0;
-        }
-        if (sortBy === 'visitCount') {
-          aValue = typeof aValue === 'number' ? aValue : 0;
-          bValue = typeof bValue === 'number' ? bValue : 0;
-        }
-        aValue = aValue ?? 0;
-        bValue = bValue ?? 0;
-        if (aValue === bValue) return 0;
-        if (direction === 'asc') return aValue > bValue ? 1 : -1;
-        return aValue < bValue ? 1 : -1;
-      })
-    );
-  };
+    const defaultSort = (bookmarks: Bookmark[]) => {
+        setBookmarks(prev => {
+            return [...prev].sort((a, b) => {
+                let aValue = a.pinned;
+                let bValue = b.pinned;
 
-  const value = useMemo(() => ({ bookmarks, setBookmarks, sortBookmarks }), [bookmarks]);
+                if (aValue === bValue) return 0;
+                return aValue < bValue ? 1 : -1;
+            });
+        });
+    }
 
-  return <BookmarkContext.Provider value={value}>{children}</BookmarkContext.Provider>;
+
+    useMemo(() => {
+        defaultSort(bookmarks);
+    }, []);
+
+    const sortBookmarks = (
+        sortBy: 'createdAt' | 'lastVisited' | 'visitCount',
+        direction: 'asc' | 'desc' = 'asc'
+    ) => {
+        setBookmarks(prev =>
+            [...prev].sort((a, b) => {
+                let aValue = a[sortBy];
+                let bValue = b[sortBy];
+
+                if (a.pinned && !b.pinned) return -1;
+                if (!a.pinned && b.pinned) return 1;
+
+                if (sortBy === 'createdAt' || sortBy === 'lastVisited') {
+                    aValue = aValue ? new Date(aValue as string).getTime() : 0;
+                    bValue = bValue ? new Date(bValue as string).getTime() : 0;
+                }
+                if (sortBy === 'visitCount') {
+                    aValue = typeof aValue === 'number' ? aValue : 0;
+                    bValue = typeof bValue === 'number' ? bValue : 0;
+                }
+
+                aValue = aValue ?? 0;
+                bValue = bValue ?? 0;
+
+                if (aValue === bValue) return 0;
+                if (direction === 'asc') return aValue > bValue ? 1 : -1;
+                return aValue < bValue ? 1 : -1;
+            })
+        );
+    }
+
+    const pinBookmark = (id: string) => {
+        setBookmarks(prev => {
+            const bookmark = prev.find(b => b.id === id);
+            if (bookmark) {
+                bookmark.pinned = true;
+            }
+            return [...prev];
+        });
+
+        defaultSort(bookmarks);
+    };
+
+    const unpinBookmark = (id: string) => {
+        setBookmarks(prev => {
+            const bookmark = prev.find(b => b.id === id);
+            if (bookmark) {
+                bookmark.pinned = false;
+            }
+            return [...prev];
+        });
+
+        defaultSort(bookmarks);
+    };
+
+    const value = useMemo(() => ({ bookmarks, setBookmarks, sortBookmarks, pinBookmark, unpinBookmark, defaultSort }), [bookmarks]);
+
+    return <BookmarkContext.Provider value={value}>{children}</BookmarkContext.Provider>;
+    
 };
 
 export function useBookmarks() {
